817576f03d660ae978a758c7b81819e844fe2576
Greg <gkg26@cornell.edu>
2021-11-21 19:58:43 -0500

    expanded training set

--- readability.py
+++ readability.py
@@ -7,27 +7,20 @@ from decouple import config
 
 openai.api_key = config('OPENAI_TOKEN')
 
-# response = openai.Completion.create(
-#     engine="davinci-codex",
-#     prompt="class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, \"a+\")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != \"\\n\":\n                f.write(\"\\n\")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event[\"_event_id\"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write(\"\\n\")\n\n    def state(self):\n        state = {\"complete\": set(), \"last\": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event[\"type\"] == \"submit\" and event[\"success\"]:\n                state[\"complete\"].add(event[\"id\"])\n                state[\"last\"] = event\n        return state\n\n\"\"\"\nHere's what the above class is doing:\n1.",
-#     temperature=0,
-#     max_tokens=64,
-#     top_p=1.0,
-#     frequency_penalty=0.0,
-#     presence_penalty=0.0,
-#     stop=["\"\"\""]
-# )
+# RUN WHEN ADDING CLASSIFICATIONS TO TRAINING SET
+# openai.File.create(file=open("project_training_data.jsonl"), purpose="classifications")
 
-# openai.File.create(file=open("trainingData.jsonl"), purpose="classifications")
-id = openai.File.list().data[0].id
+num_ids = len(openai.File.list().data)
+print(num_ids)
+id = openai.File.list().data[num_ids - 1].id
 print(id)
 
+# TEST SET
 response = openai.Classification.create(
     file=id,
-    query="movie is very bad",
+    query="def add_1(arr):\n  return map(lambda x: x + 1, arr)",
     search_model="ada", 
-    model="davinci-codex", 
-    max_examples=3
+    model="davinci-codex"
 )
 
 print(response)
