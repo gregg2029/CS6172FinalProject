ca2c714dd34626f1faeb872940b5fe3093c48a74
Greg <gkg26@cornell.edu>
2021-11-29 21:15:11 -0500

    Crazy stuff, doesn't work tho

--- readability.py
+++ readability.py
@@ -15,59 +15,64 @@ labels = ["Readable", "Acceptable", "Difficult", "Unreadable"]
 labels = [label.strip().lower().capitalize() for label in labels]
 
 # TEST SET
-result = openai.Classification.create(
-    query="def add_1(arr):\n  return map(lambda x: x + 1, arr)",
-    search_model="ada", 
-    model="davinci-codex",
-    logprobs=5,
-    labels=labels,
-    max_examples=2,
-    examples=[
-        ["def contains(item, arr):\n    return item in arr", "Readable"],
-        ["def find_object(target_obj, arr):\n    for item in arr:\n        if item == target_obj:\n            return True\n\n    return False", "Acceptable"],
-        ["def elem_in_list(element, check_list):\n  len_list = len(check_list)\n  for ind in range(len_list):\n    if check_list[ind] == element:\n      return True\n  \n  return False", "Difficult"],
-        ["def obj_in_array(target_obj, arr):\n    return next(filter(lambda arr_item: arr_item == target_obj, arr), None) != None", "Unreadable"],
-        ["def add_one(arr):\n  new_arr = []\n  for elem in arr:\n    new_elem = elem + 1\n    new_arr.append(new_elem)\n  return new_arr", "Readable"],
-        ["def add_one(arr):\n  new_arr = []\n  for elem in arr:\n    new_arr.append(elem + 1)\n  return new_arr", "Acceptable"],
-        ["def add_one(arr):\n  return [elem + 1 for elem in arr]", "Acceptable"],
-        ["def add_one(arr):\n  return map(lambda x: x + 1, arr)", "Difficult"],
-    ]
-)
 
-# Take the starting tokens for probability estimation.
-# Labels should have distinct starting tokens.
-# Here tokens are case-sensitive.
-print("result: ", result)
-labels = [" " + label for label in labels]
-top_logprobs = result["completion"]["choices"][0]["logprobs"]["top_logprobs"][1]
-print("top_logprobs: ", top_logprobs)
-print("results: ", result)
-probs = {
-    sublabel: np.exp(logp) 
-    for sublabel, logp in top_logprobs.items()
-}
-label_probs = {}
-for sublabel, prob in probs.items():
-    for label in labels:
-        if sublabel in label:
-            label_probs[label] = prob
+def classifier(query):
+    result = openai.Classification.create(
+        query=query,
+        search_model="ada", 
+        model="davinci-codex",
+        logprobs=5,
+        labels=labels,
+        max_examples=2,
+        examples=[
+            ["def contains(item, arr):\n    return item in arr", "Readable"],
+            ["def find_object(target_obj, arr):\n    for item in arr:\n        if item == target_obj:\n            return True\n\n    return False", "Acceptable"],
+            ["def elem_in_list(element, check_list):\n  len_list = len(check_list)\n  for ind in range(len_list):\n    if check_list[ind] == element:\n      return True\n  \n  return False", "Difficult"],
+            ["def obj_in_array(target_obj, arr):\n    return next(filter(lambda arr_item: arr_item == target_obj, arr), None) != None", "Unreadable"],
+            ["def add_one(arr):\n  new_arr = []\n  for elem in arr:\n    new_elem = elem + 1\n    new_arr.append(new_elem)\n  return new_arr", "Readable"],
+            ["def add_one(arr):\n  new_arr = []\n  for elem in arr:\n    new_arr.append(elem + 1)\n  return new_arr", "Acceptable"],
+            ["def add_one(arr):\n  return [elem + 1 for elem in arr]", "Acceptable"],
+            ["def add_one(arr):\n  return map(lambda x: x + 1, arr)", "Difficult"],
+        ]
+    )
+
+    return result
+
+def cost(classification):
+
+    # Take the starting tokens for probability estimation.
+    # Labels should have distinct starting tokens.
+    # Here tokens are case-sensitive.
+    labels = [" " + label for label in labels]
+    top_logprobs = classification["completion"]["choices"][0]["logprobs"]["top_logprobs"][1]
+
+    probs = {
+        sublabel: np.exp(logp) 
+        for sublabel, logp in top_logprobs.items()
+    }
+    label_probs = {}
+    for sublabel, prob in probs.items():
+        for label in labels:
+            if sublabel in label:
+                label_probs[label] = prob
 
 
-# Fill in the probability for the special "Unknown" label.
-if sum(label_probs.values()) < 1.0:
-    label_probs[" Unknown"] = 1.0 - sum(label_probs.values())
+    # Fill in the probability for the special "Unknown" label.
+    if sum(label_probs.values()) < 1.0:
+        label_probs[" Unknown"] = 1.0 - sum(label_probs.values())
+    
+    for label_prob in label_probs.keys():
+        print(label_prob, ": ", label_probs[label_prob])
 
-label_weights = {}
-label_weights[" Readable"] = 0.1
-label_weights[" Acceptable"] = 1
-label_weights[" Difficult"] = 10
-label_weights[" Unreadable"] = 100
-label_weights[" Unknown"] = 25
+    label_weights = {}
+    label_weights[" Readable"] = 0.1
+    label_weights[" Acceptable"] = 1
+    label_weights[" Difficult"] = 10
+    label_weights[" Unreadable"] = 100
+    label_weights[" Unknown"] = 25
 
-cost = 0
-for label in label_probs.keys():
-    cost += label_probs[label] * label_weights[label]
+    cost = 0
+    for label in label_probs.keys():
+        cost += label_probs[label] * label_weights[label]
 
-print("Cost: ", cost)
-
-print("Labels: ", label_probs)
+    return cost
--- synthesizer.py
+++ synthesizer.py
@@ -1,15 +1,32 @@
+# To activate env run: $ source activate cs6172_env
+# To deactivate env run $ conda deactivate
+
 import os
+import torch
 import openai
+from decouple import config
+# from collections import defaultdict
+import numpy as np
+from readability import *
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  engine="davinci",
-  prompt="Create a python function that adds one to each element in a list :\n\ndef",
-  temperature=0.3,
-  max_tokens=60,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\n"]
-)
+openai.api_key = config('OPENAI_TOKEN')
+
+
+for x in range(10):
+  response = openai.Completion.create(
+    engine="davinci",
+    prompt="Create a python function that adds one to each element in a list :\n\ndef",
+    temperature=1,
+    max_tokens=60,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\n"]
+  )
+
+  code = response.choices[0].text
+  classification = classifier(code)
+  cost = cost(classification)
+
+  print("Code: ", code, "\n\tScore: ", cost)
