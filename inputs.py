4e9c96dcb9f1e0c979d6b077b2ff4c802e2ed59e
Hahnbee Lee <hl985@cornell.edu>
2021-12-03 22:16:09 -0500

    add greg's tests

--- inputs.py
+++ inputs.py
@@ -1,7 +1,7 @@
 # add dictionaries with the properties query, type, and test_cases to synthesize new problems
 inputs = [
   {
-    "query": "Create a python function that finds if an item is in an int array",
+    "query": "Create a java function that finds if an item is in an int array",
     "type": "boolean",
     "test_cases": [
       ({"int[]": "new int[] {1, 2, 3, 4}", "int": "3"}, "true"),
@@ -9,6 +9,26 @@ inputs = [
       ({"int[]": "new int[] {1, 2, 3, 4}", "int": "4"}, "true"),
       ({"int[]": "new int[] {1, 2, 3, 4, 4, 4}", "int": "4"}, "true"),
       ({"int[]": "new int[] {1, 2, 3, 4, 4, 4}", "int": "6"}, "false")
+    ]
+  },
+  {
+    "query": "Create a java function that returns the nth value of the fibonacci sequence",
+    "type": "int",
+    "test_cases": [
+      ({"int": "3"}, "2"),
+      ({"int": "6"}, "8"),
+      ({"int": "1"}, "1"),
+      ({"int": "2"}, "1"),
+      ({"int": "5"}, "5")
+    ]
+  },
+  {
+    "query": "Create a java function that reverses an input string",
+    "type": "String",
+    "test_cases": [
+      ({"String": "\"hello\""}, "olleh"),
+      ({"String": "\"xyx\""}, "xyx"),
+      ({"String": "\"hey there\""}, "ereht yeh")
     ]
   }
 
