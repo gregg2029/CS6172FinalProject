d420f35d5e1fe10cfbb4e2696d9ea142cf54b8d6
Hahnbee Lee <hl985@cornell.edu>
2021-12-05 18:57:15 -0500

    define length more!

--- data/features.py
+++ data/features.py
@@ -1,11 +1,23 @@
 import openai
 
-def numBrackets(code):
+def numChar(code, char):
   count = 0
   for character in code:
-    if character == "{":
+    if character == char:
       count = count + 1
   return count
+
+def numBrackets(code):
+  return numChar(code, "{")
+
+def numParen(code):
+  return numChar(code, "(")
+
+def numSemicolan(code):
+  return numChar(code, ";")
+
+def lengthOfCode(code):
+  return numBrackets(code) + numParen(code) + numSemicolan(code)
 
 def timeComplexity(code):
   response = openai.Completion.create(
